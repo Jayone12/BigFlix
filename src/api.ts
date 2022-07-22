21ff9afc82fa46dc24441630aba2acc5e54bf893
BigJay <53748573+Jayone12@users.noreply.github.com>
2022-07-22 19:20:01 +0900

    feat: 영화 리스트 API 생성

---
+++ src/api.ts
@@ -0,0 +1,8 @@
+const API_KEY = "8f713df908558b2d2df68ce857282c98";
+const BASE_PATH = "https://api.themoviedb.org/3";
+
+export function getMovies() {
+  return fetch(`${BASE_PATH}/movie/now_playing?api_key=${API_KEY}`).then(
+    (response) => response.json()
+  );
+}
