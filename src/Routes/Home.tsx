efa59c63750d2ad348927788cd0a0bb0214d207f
BigJay <53748573+Jayone12@users.noreply.github.com>
2022-07-22 19:38:58 +0900

    feat: react query를 통하여 영화 데이터 호출

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -1,4 +1,22 @@
+import { useQuery } from "@tanstack/react-query";
+import styled from "styled-components";
+import { getMovies, IgetMoviesResult } from "../api";
+
+const Wrapper = styled.div``;
+
+const Loader = styled.div`
+  height: 20vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
 function Home() {
-  return <div style={{ height: "200vh" }}></div>;
+  const { data, isLoading } = useQuery<IgetMoviesResult>(
+    ["movies", "nowPlaying"],
+    getMovies
+  );
+  console.log(data, isLoading);
+  return <Wrapper>{isLoading ? <Loader>Loading...</Loader> : null}</Wrapper>;
 }
 export default Home;
