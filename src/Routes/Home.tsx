c99b48fa5c0887eb66614fd2948e3cb7fcf1bb39
BigJay <53748573+Jayone12@users.noreply.github.com>
2022-07-24 17:16:15 +0900

    style: 영화 마우스 오버시의 타이틀 내용 스타일 작성

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -1,6 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
 import { motion, AnimatePresence } from "framer-motion";
 import { useEffect, useState } from "react";
+import { useNavigate } from "react-router";
 import styled from "styled-components";
 import { getMovies, IgetMoviesResult } from "../api";
 import { makeImagePath } from "../utils";
@@ -54,7 +55,26 @@ const Box = styled(motion.div)<{ $bgPhoto: string }>`
   background-size: cover;
   background-position: center center;
   height: 200px;
-  color: red;
+  position: relative;
+  &:first-child {
+    transform-origin: center left;
+  }
+  &:last-child {
+    transform-origin: center right;
+  }
+`;
+
+const Info = styled(motion.div)`
+  padding: 20px;
+  background-color: ${(props) => props.theme.black.lighter};
+  opacity: 0;
+  position: absolute;
+  width: 100%;
+  bottom: 0;
+  h4 {
+    text-align: center;
+    font-size: 18px;
+  }
 `;
 
 const rowVariants = {
@@ -69,9 +89,35 @@ const rowVariants = {
   },
 };
 
+const boxVariants = {
+  nomal: {
+    scale: 1,
+  },
+  hover: {
+    zIndex: 99,
+    scale: 1.3,
+    y: -50,
+    transition: {
+      delay: 0.6,
+      type: "tween",
+    },
+  },
+};
+
+const infoVariants = {
+  hover: {
+    opacity: 1,
+    transition: {
+      delay: 0.6,
+      type: "tween",
+    },
+  },
+};
+
 let offset: number = 6;
 
 function Home() {
+  const history = useNavigate();
   const { data, isLoading } = useQuery<IgetMoviesResult>(
     ["movies", "nowPlaying"],
     getMovies
@@ -89,9 +135,10 @@ function Home() {
     }
   };
 
-  console.log(offset);
-
   const toggleLeaving = () => setLeaving((prev) => !prev);
+  const onBoxClicked = (movieId: number) => {
+    history(`/movies/${movieId}`);
+  };
 
   return (
     <Wrapper>
@@ -121,10 +168,17 @@ function Home() {
                   .slice(offset * index, offset * index + offset)
                   .map((movie) => (
                     <Box
+                      variants={boxVariants}
+                      whileHover="hover"
+                      initial="nomal"
+                      transition={{ type: "tween" }}
                       key={movie.id}
                       $bgPhoto={makeImagePath(movie.backdrop_path, "w500")}
+                      onClick={() => onBoxClicked(movie.id)}
                     >
-                      {movie.title}
+                      <Info variants={infoVariants}>
+                        <h4>{movie.title}</h4>
+                      </Info>
                     </Box>
                   ))}
               </Row>
